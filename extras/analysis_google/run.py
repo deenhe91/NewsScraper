270ffdd7d3b58eaf135727e20252a7d935391c27
deenhe91 <hannah.deen91@gmail.com>
2017-03-11 11:09:50 +0100

    Delete run.py

--- extras/analysis_google/run.py
+++
@@ -1,7 +0,0 @@
-#!flask/bin/python
-
-from app import app
-app.run(debug=True)
-
-# have interface where user can input document, url perhaps?
-# app processes content of document and returns topics and sentiment.
